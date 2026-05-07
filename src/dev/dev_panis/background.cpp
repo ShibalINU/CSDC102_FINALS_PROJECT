6ae62ee49f7fdfecb0345be484bb6d9621c5f8c1
Gelo <mapanis@gbox.adnu.edu.ph>
2026-05-07 15:39:16 +0800

    Update background.cpp

--- src/dev/dev_panis/background.cpp
+++ src/dev/dev_panis/background.cpp
@@ -1,1 +1,20 @@
+#include <windows.h>
+#include <mmsystem.h>
+#include <iostream>
+#pragma comment(lib, "winmm.lib")
 
+using namespace std;
+
+int main (){
+
+
+    cout << "========= CHAYKIN RUDE KRUSIIG =========" << endl;
+    cout << "Playing music...\n";
+    PlaySound("songs/Stardew.wav", NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+    cout << "Press Enter to Stop..\n";
+    cin.get();
+
+    PlaySound(NULL, NULL, 0);
+
+    return 0;
+}
